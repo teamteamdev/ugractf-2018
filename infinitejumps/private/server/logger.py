2f56104380be4d329095939d85f89ed22df77f3c
Nikita Sychev <root@nsychev.ru>
2018-06-04 01:22:13 +0300

    cut logs

--- infinitejumps/private/server/logger.py
+++ infinitejumps/private/server/logger.py
@@ -6,6 +6,10 @@ LOG_PATH = "/tmp/gameserver/log.txt"
 def log(data, out=False):
     ip = os.getenv("SOCAT_PEERADDR", "???.???.???.???")
     f = open(LOG_PATH, "a")
+    
+    if out and len(data) > 30:
+        data = data[:30] + "..."
+    
     print("{} - {} {} {}".format(
         str(datetime.now()), ip, "<" if out else ">", data
     ), file=f)
